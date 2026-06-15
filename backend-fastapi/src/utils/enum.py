cf1296a991882d51efd6e3b61cbb473782e392a1
Александр Маринушкин <marisasha228@bk.ru>
2026-06-15 13:38:53 +0300

    create: order status enum

--- backend-fastapi/src/utils/enum.py
+++ backend-fastapi/src/utils/enum.py
@@ -17,3 +17,25 @@ class GenderEnum(str, Enum):
 
     def __str__(self):
         return self.value
+
+
+class OrderStatusEnum(str, Enum):
+    PENDING = "PENDING"
+    PAID = "PAID"
+    PROCESSING = "PROCESSING"
+    SHIPPED = "SHIPPED"
+    DELIVERED = "DELIVERED"
+    COMPLETED = "COMPLETED"
+    CANCELLED = "CANCELLED"
+    FAILED = "FAILED"
+
+    def __str__(self):
+        return self.value
+
+
+class OperationEnum(str, Enum):
+    ADD = "ADD"
+    REMOVE = "REMOVE"
+
+    def __str__(self):
+        return self.value
