debdf269e300dd88280dd4420bf64be1cac5b41d
Александр Маринушкин <marisasha228@bk.ru>
2026-06-15 13:30:55 +0300

    update: add strore router and sales_panel_router

--- backend-fastapi/src/router.py
+++ backend-fastapi/src/router.py
@@ -2,9 +2,12 @@ from fastapi import APIRouter
 
 from src.auth.service import router as auth_router
 from src.user.service import router as user_router
+from src.sales_panel.service import router as sales_panel_router
+from src.store.service import router as store_router
 
 main_router = APIRouter()
 
 main_router.include_router(auth_router)
 main_router.include_router(user_router)
-
+main_router.include_router(sales_panel_router)
+main_router.include_router(store_router)
