400f435264759a02a76f8149d87a2bcc028ca2bd
Александр Маринушкин <marisasha228@bk.ru>
2026-06-18 17:46:34 +0300

    create: yuokassa config

--- backend-fastapi/src/config.py
+++ backend-fastapi/src/config.py
@@ -13,56 +13,64 @@ class DBConfig(BaseModel):
     def url(self):
         return f"postgresql+asyncpg://postgres:{self.password}@{self.host}:{self.port}/{self.name}"
 
+
 class JWTConfig(BaseModel):
     secret_key: str
-    algorithm: str 
-    access_token_expire_minutes: int 
-    refresh_token_expire_days: int 
-
-# class RedisConfig(BaseModel):
-#     host: str
-#     port: int
-#     db: int
+    algorithm: str
+    access_token_expire_minutes: int
+    refresh_token_expire_days: int
 
 
-# class CacheConfig(BaseModel):
-#     prefix: str
-#     expire_seconds: int
+class YookassaConfig(BaseModel):
+    secret_key: str
+    shop_id: str
 
 
-# class RabbitConfig(BaseModel):
-#     host: str
-#     port: int
-#     user: str
-#     password: str
-#     vhost: str
-#     ssl: bool
-
-#     @property
-#     def url(self):
-#         scheme = "amqps" if self.ssl else "amqp"
-#         return f"{scheme}://{self.user}:{self.password}@{self.host}:{self.port}/{self.vhost}"
+class RedisConfig(BaseModel):
+    host: str
+    port: int
+    db: int
 
 
-# class EmailConfig(BaseModel):
-#     host: str
-#     port: int
-#     user: str
-#     password: str
+class CacheConfig(BaseModel):
+    prefix: str
+    expire_seconds: int
+
+
+class RabbitConfig(BaseModel):
+    host: str
+    port: int
+    user: str
+    password: str
+    vhost: str
+    ssl: bool
+
+    @property
+    def url(self):
+        scheme = "amqps" if self.ssl else "amqp"
+        return f"{scheme}://{self.user}:{self.password}@{self.host}:{self.port}/{self.vhost}"
+
+
+class EmailConfig(BaseModel):
+    host: str
+    port: int
+    user: str
+    password: str
 
 
 class Settings(BaseSettings):
     db: DBConfig
     jwt: JWTConfig
-    # redis: RedisConfig
-    # cache: CacheConfig
-    # rabbit: RabbitConfig
-    # email: EmailConfig
+    yookassa: YookassaConfig
+    redis: RedisConfig
+    cache: CacheConfig
+    rabbit: RabbitConfig
+    email: EmailConfig
 
     class Config:
         env_file = ".env"
         env_nested_delimiter = "__"
-        extra='ignore'
+        extra = "ignore"
 
 
 settings = Settings()
